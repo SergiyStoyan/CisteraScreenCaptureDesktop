fca008f95a66fcd78dfa8d12db23345004301832
cliversoft <sergey.stoyan@gmail.com>
2017-11-03 13:56:28 +0200

    .

--- CisteraScreenCaptureTestServer/MainForm.cs
+++ CisteraScreenCaptureTestServer/MainForm.cs
@@ -24,8 +24,32 @@ namespace Cliver.CisteraScreenCaptureTestServer
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, int.Parse(localPort.Text));
             socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             socket.Bind(localEndPoint);
+            
+            // URI prefixes are required,
+            // for example "http://127.0.0.1:5800/screenCapture/".
+            string[] prefixes = new string[] { "http://127.0.0.1:5800/screenCapture/" };
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+            HttpListener listener = new HttpListener();
+            foreach (string s in prefixes)
+                listener.Prefixes.Add(s);
+            listener.Start();
+            listener.BeginGetContext(http_callback, listener);
+
+            FormClosed += delegate
+              {
+                  try
+                  {
+                      listener.Stop();
+                  }
+                  catch
+                  {
+
+                  }
+              };
         }
         Socket socket;
+        HttpListener listener;
 
         private void start_Click(object sender, EventArgs e)
         {
@@ -45,6 +69,20 @@ namespace Cliver.CisteraScreenCaptureTestServer
             TcpMessage m2 = m.SendAndReceiveReply(socket);
 
             socket.Disconnect(true);
+        }
+
+        void http_callback(System.IAsyncResult result)
+        {
+            HttpListener listener = (HttpListener)result.AsyncState;
+            HttpListenerContext context = listener.EndGetContext(result);
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+            string responseString = "OK";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
         }
     }
 }
