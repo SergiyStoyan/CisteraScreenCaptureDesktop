d5da08da94af28a71326659261c4cfe38b94a9db
cliversoft <sergey.stoyan@gmail.com>
2017-11-04 12:13:20 +0200

    .

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Net;
 using System.IO;
 using System.Management;
 using System.Threading;
+using System.Windows.Media.Animation;
+
 
 namespace Cliver.CisteraScreenCapture
 {
@@ -29,8 +31,38 @@ namespace Cliver.CisteraScreenCapture
 
             Icon = AssemblyRoutines.GetAppIconImageSource();
 
+            ContentRendered += delegate
+            {
+                this.MinHeight = this.ActualHeight;
+                this.MaxHeight = this.ActualHeight;
+                this.MinWidth = this.ActualWidth;
+             };
+
+            IsVisibleChanged += (object sender, DependencyPropertyChangedEventArgs e) =>
+            {
+                if(Visibility == Visibility.Visible)
+                {
+                    DoubleAnimation da = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(300));
+                    this.BeginAnimation(UIElement.OpacityProperty, da);
+                }
+            };
+
+            Closing += (object sender, System.ComponentModel.CancelEventArgs e) =>
+            {
+                if (Opacity > 0)
+                {
+                    e.Cancel = true;
+                    DoubleAnimation da = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(200));
+                    da.Completed += delegate { Close(); };
+                    this.BeginAnimation(UIElement.OpacityProperty, da);
+                }
+            };
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
             //WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            
+            //DefaultServerIp.ValueDataType = typeof(IPAddress);
+
             ServerPort.Text = Settings.General.TcpClientPort.ToString();
             DefaultServerIp.Text = Settings.General.DefaultTcpClientIp.ToString();
             ClientPort.Text = Settings.General.TcpServerPort.ToString();
@@ -65,18 +97,18 @@ namespace Cliver.CisteraScreenCapture
                 ushort v;
 
                 if (!ushort.TryParse(ServerPort.Text, out v))
-                    throw new Exception("Server port must be positive integer.");
+                    throw new Exception("Server port must be an integer between 0 and " + ushort.MaxValue);
                 Settings.General.TcpClientPort = v;
 
                 if (string.IsNullOrWhiteSpace(DefaultServerIp.Text))
                     throw new Exception("Default server ip is not specified.");
                 IPAddress ia;
                 if(!IPAddress.TryParse(DefaultServerIp.Text, out ia))
-                    throw new Exception("Default server ip could not be parsed.");
-                Settings.General.DefaultTcpClientIp = ia.ToString(); ;
+                    throw new Exception("Default server ip is not a valid value.");
+                Settings.General.DefaultTcpClientIp = ia.ToString(); 
 
                 if (!ushort.TryParse(ClientPort.Text, out v))
-                    throw new Exception("Client port must be positive integer.");
+                    throw new Exception("Client port must be an between 0 and " + ushort.MaxValue);
                 Settings.General.TcpServerPort = v;
 
                 Settings.General.Ssl = Ssl.IsChecked ?? false;
