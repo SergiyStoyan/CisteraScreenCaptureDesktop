38eaf57fc98840ce8e35fd94ab1ee2e5f5a60f78
cliversoft <sergey.stoyan@gmail.com>
2017-11-10 12:21:03 +0200

    .

--- CisteraScreenCaptureTestServer/MainForm.Designer.cs
+++ CisteraScreenCaptureTestServer/MainForm.Designer.cs
@@ -30,17 +30,18 @@
         {
             this.start = new System.Windows.Forms.Button();
             this.stop = new System.Windows.Forms.Button();
-            this.localPort = new System.Windows.Forms.TextBox();
+            this.localTcpPort = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.remotePort = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.remoteHost = new System.Windows.Forms.TextBox();
+            this.mpegCommandLine = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.state = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // start
             // 
-            this.start.Location = new System.Drawing.Point(144, 164);
+            this.start.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.start.Location = new System.Drawing.Point(369, 312);
             this.start.Name = "start";
             this.start.Size = new System.Drawing.Size(75, 23);
             this.start.TabIndex = 0;
@@ -50,7 +51,8 @@
             // 
             // stop
             // 
-            this.stop.Location = new System.Drawing.Point(144, 193);
+            this.stop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.stop.Location = new System.Drawing.Point(450, 312);
             this.stop.Name = "stop";
             this.stop.Size = new System.Drawing.Size(75, 23);
             this.stop.TabIndex = 1;
@@ -58,68 +60,76 @@
             this.stop.UseVisualStyleBackColor = true;
             this.stop.Click += new System.EventHandler(this.stop_Click);
             // 
-            // localPort
+            // localTcpPort
             // 
-            this.localPort.Location = new System.Drawing.Point(99, 24);
-            this.localPort.Name = "localPort";
-            this.localPort.Size = new System.Drawing.Size(67, 20);
-            this.localPort.TabIndex = 2;
-            this.localPort.Text = "5700";
+            this.localTcpPort.Location = new System.Drawing.Point(112, 17);
+            this.localTcpPort.Name = "localTcpPort";
+            this.localTcpPort.Size = new System.Drawing.Size(67, 20);
+            this.localTcpPort.TabIndex = 2;
+            this.localTcpPort.Text = "5700";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(24, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.Size = new System.Drawing.Size(82, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Local Port:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 50);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Remote Port:";
-            // 
-            // remotePort
-            // 
-            this.remotePort.Location = new System.Drawing.Point(99, 50);
-            this.remotePort.Name = "remotePort";
-            this.remotePort.Size = new System.Drawing.Size(67, 20);
-            this.remotePort.TabIndex = 4;
-            this.remotePort.Text = "5900";
+            this.label1.Text = "Local TCP Port:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 76);
+            this.label3.Location = new System.Drawing.Point(24, 164);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Remote Host:";
+            this.label3.Text = "State:";
             // 
-            // remoteHost
+            // mpegCommandLine
             // 
-            this.remoteHost.Location = new System.Drawing.Point(99, 76);
-            this.remoteHost.Name = "remoteHost";
-            this.remoteHost.Size = new System.Drawing.Size(102, 20);
-            this.remoteHost.TabIndex = 6;
-            this.remoteHost.Text = "127.0.0.1";
+            this.mpegCommandLine.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mpegCommandLine.Location = new System.Drawing.Point(27, 89);
+            this.mpegCommandLine.Multiline = true;
+            this.mpegCommandLine.Name = "mpegCommandLine";
+            this.mpegCommandLine.Size = new System.Drawing.Size(498, 62);
+            this.mpegCommandLine.TabIndex = 8;
+            this.mpegCommandLine.Text = "-f gdigrab -framerate 10 -f rtp_mpegts -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -s" +
+    "rtp_out_params aMg7BqN047lFN72szkezmPyN1qSMilYCXbqP/sCt srtp://127.0.0.1:5920";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Mpeg Command Line:";
+            // 
+            // state
+            // 
+            this.state.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.state.Location = new System.Drawing.Point(27, 180);
+            this.state.Multiline = true;
+            this.state.Name = "state";
+            this.state.Size = new System.Drawing.Size(498, 117);
+            this.state.TabIndex = 10;
+            this.state.Text = "gddgrdfg";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(559, 366);
+            this.Controls.Add(this.state);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.mpegCommandLine);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.remoteHost);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.remotePort);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.localPort);
+            this.Controls.Add(this.localTcpPort);
             this.Controls.Add(this.stop);
             this.Controls.Add(this.start);
             this.Name = "MainForm";
@@ -133,11 +143,11 @@
 
         private System.Windows.Forms.Button start;
         private System.Windows.Forms.Button stop;
-        private System.Windows.Forms.TextBox localPort;
+        private System.Windows.Forms.TextBox localTcpPort;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox remotePort;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox remoteHost;
+        private System.Windows.Forms.TextBox mpegCommandLine;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox state;
     }
 }
--- CisteraScreenCaptureTestServer/MainForm.cs
+++ CisteraScreenCaptureTestServer/MainForm.cs
@@ -10,7 +10,10 @@ using System.Windows.Forms;
 using System.Net.Sockets;
 using System.Net;
 using Cliver.CisteraScreenCapture;
-using Zeroconf;
+using Mono.Zeroconf;
+using Bonjour;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace Cliver.CisteraScreenCaptureTestServer
 {
@@ -20,24 +23,55 @@ namespace Cliver.CisteraScreenCaptureTestServer
         {
             InitializeComponent();
 
+            mpegCommandLine.Text = "-f gdigrab -framerate 10 -f rtp_mpegts -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params aMg7BqN047lFN72szkezmPyN1qSMilYCXbqP/sCt srtp://127.0.0.1:5920";
+
+            start.Enabled = false;
+            stop.Enabled = false;
+            
             HttpListener listener = new HttpListener();
             try
             {
                 // URI prefixes are required,
                 // for example "http://127.0.0.1:5800/screenCapture/" (does not work in LAN)
                 //listener.Prefixes.Add("http://192.168.2.15:80/");
-                listener.Prefixes.Add("http://127.0.0.1:80/");
-                listener.Prefixes.Add("http://localhost:80/");
+                //listener.Prefixes.Add("http://127.0.0.1:80/");
+                //listener.Prefixes.Add("http://localhost:80/");
+                listener.Prefixes.Add("http://*:80/");
                 listener.Start();
                 listener.BeginGetContext(http_callback, listener);
+
+                state.Text = "Wating for HTTP request...";
             }
             catch(Exception e)
             {
-
+                Message.Error(e);
             }
-            
-            //Zeroconf..
 
+            //ServiceBrowser browser = new ServiceBrowser();
+            //browser.ServiceAdded += delegate (object o, ServiceBrowseEventArgs args) {
+            //    Console.WriteLine("Found Service: {0}", args.Service.Name);
+            //    args.Service.Resolved += delegate (object o2, ServiceResolvedEventArgs args2) {
+            //        IResolvableService s = (IResolvableService)args2.Service;
+            //        Console.WriteLine("Resolved Service: {0} - {1}:{2} ({3} TXT record entries)",
+            //            s.FullName, s.HostEntry.AddressList[0], s.Port, s.TxtRecord.Count);
+            //    };
+            //    args.Service.Resolve();
+            //};
+            //browser.Browse("_daap._tcp", "local");
+
+            //RegisterService service = new RegisterService();
+            //service.Name = "Aaron's DAAP Share";
+            //service.RegType = "_daap._tcp";
+            //service.ReplyDomain = "local.";
+            //service.Port = 3689;
+            //// TxtRecords are optional
+            //TxtRecord txt_record = new TxtRecord();
+            //txt_record.Add("Password", "false");
+            //service.TxtRecord = txt_record;
+            //service.Register();
+            
+            // int t = DNSServiceRegister(ref IntPtr.Zero, 0, 0, "test",, "_cisterascreencapturecontroller._tcp", null, null, 5353, 0, null, IntPtr.Zero, IntPtr.Zero);
+            
             FormClosed += delegate
               {
                   try
@@ -52,52 +86,186 @@ namespace Cliver.CisteraScreenCaptureTestServer
         }
         Socket socket;
 
-        private void start_Click(object sender, EventArgs e)
-        { 
+        //     [DllImport("dnssd.dll", SetLastError = true)]
+        //     public static extern Int32 DNSServiceRegister(
+        //   ref  HandleRef sdRef, //DNSServiceRef* sdRef,
+        //Int32 flags, //DNSServiceFlags flags,
+        //UInt32 interfaceIndex, //uint32_t interfaceIndex,
+        //string name, //const char* name,         /* may be NULL */
+        //string regtype, //const char* regtype,
+        //string domain, //const char* domain,       /* may be NULL */
+        //string host, //const char* host,         /* may be NULL */
+        //UInt16 port, //uint16_t                            port,          /* In network byte order */
+        //UInt16 txtLen, //uint16_t txtLen,
+        //string txtRecord, //const void* txtRecord,    /* may be NULL */
+        // IntPtr callBack, //DNSServiceRegisterReply             callBack,      /* may be NULL */
+        //IntPtr context //void* context       /* may be NULL */
+        // );
+
+
+        //       typedef void (DNSSD_API* DNSServiceRegisterReply)
+        //   (
+        //   DNSServiceRef sdRef,
+        //   DNSServiceFlags flags,
+        //   DNSServiceErrorType errorCode,
+        //   const char* name,
+        //   const char* regtype,
+        //   const char* domain,
+        //   void* context
+        //   );
+
+        //       static void DNSSD_API serviceRegisterReply(
+        //       DNSServiceRef sdRef,
+        //       DNSServiceFlags flags,
+        //       DNSServiceErrorType errorCode,
+
+        //       const char* name,
+
+        //       const char* regtype,
+
+        //       const char* domain,
+
+        //       void* context
+        //)
+        //{
+
+        //       TCHAR name_[255];
+
+        //       mbstowcs(name_, name, sizeof(name_));
+        //	if (errorCode == kDNSServiceErr_NoError)
+        //	{
+        //		if (flags & kDNSServiceFlagsAdd)
+        //			log->info(_T("BonjourService: Service %s is registered and active."), name_);
+        //		else
+        //			log->info(_T("BonjourService: Service %s is unregistered."), name_);
+        //	}
+        //	else if (errorCode == kDNSServiceErr_NameConflict)
+        //		log->error(_T("BonjourService: Service name %s is in use, please choose another."), name_);
+        //	else
+        //		log->error(_T("BonjourService: Error: %d"), errorCode);
+        //}
+
+        void connect_socket()
+        {
             //IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
             //IPAddress ipAddress = ipHostInfo.AddressList[0];
             IPAddress ipAddress = NetworkRoutines.GetLocalIpForDestination(IPAddress.Parse("127.0.0.1"));
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, int.Parse(localPort.Text));
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, int.Parse(localTcpPort.Text));
             socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             socket.Bind(localEndPoint);
 
-            socket.Connect(remoteHost.Text, int.Parse(remotePort.Text));
+            socket.Connect(remoteHost, int.Parse(remotePort));
+        }
 
-            TcpMessage m = new TcpMessage(TcpMessage.FfmpegStart, "-f gdigrab -framerate 10 -f rtp_mpegts -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params aMg7BqN047lFN72szkezmPyN1qSMilYCXbqP/sCt srtp://127.0.0.1:5920");
-            TcpMessage m2 = m.SendAndReceiveReply(socket);
-
+        void disconnect_socket()
+        {
             socket.Disconnect(false);
             socket.Close();
             socket = null;
         }
 
+        private void start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                state.Text = "Starting MPEG...";
+
+                connect_socket();
+                TcpMessage m = new TcpMessage(TcpMessage.FfmpegStart, mpegCommandLine.Text);
+                TcpMessage m2 = m.SendAndReceiveReply(socket);
+
+                //Message.Inform("Response: " + m2.BodyAsText);
+                state.Text = "MPEG started";
+                start.Enabled = false;
+                stop.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                Message.Error(ex);
+            }
+            finally
+            {
+                disconnect_socket();
+            }
+        }
+
         private void stop_Click(object sender, EventArgs e)
         {
-            socket.Connect(remoteHost.Text, int.Parse(remotePort.Text));
+            try
+            {
+                state.Text = "Stopping MPEG...";
 
-            TcpMessage m = new TcpMessage(TcpMessage.FfmpegStop, null);
-            TcpMessage m2 = m.SendAndReceiveReply(socket);
-
-            socket.Disconnect(true);
+                connect_socket();
+                TcpMessage m = new TcpMessage(TcpMessage.FfmpegStop, null);
+                TcpMessage m2 = m.SendAndReceiveReply(socket);
+                
+                //Message.Inform("Response: " + m2.BodyAsText);
+                state.Text = "MPEG stopped";
+                start.Enabled = true;
+                stop.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                Message.Error(ex);
+            }
+            finally
+            {
+                disconnect_socket();
+            }
         }
 
         void http_callback(System.IAsyncResult result)
         {
+            string responseString;
+            string username = null;
             HttpListener listener = (HttpListener)result.AsyncState;
             HttpListenerContext context = listener.EndGetContext(result);
-            HttpListenerRequest request = context.Request;
-            HttpListenerResponse response = context.Response;
-            string responseString = "OK";
+            try
+            {
+                HttpListenerRequest request = context.Request;
+                Match m = Regex.Match(request.Url.Query, @"username=(.+?)(&|$)");
+                if (!m.Success)
+                    throw new Exception("No username in http request.");
+                username = m.Groups[1].Value;
+
+                m = Regex.Match(request.Url.Query, @"ipaddress=(.+?)(&|$)");
+                if (!m.Success)
+                    throw new Exception("No ipaddress in http request.");
+                remoteHost = m.Groups[1].Value;
+
+                m = Regex.Match(request.Url.Query, @"port=(.+?)(&|$)");
+                if (!m.Success)
+                    throw new Exception("No port in http request.");
+                remotePort = m.Groups[1].Value;
+
+                responseString = "OK";
+            }
+            catch (Exception e)
+            {
+                responseString = Message.GetExceptionDetails(e);
+            }
+
             byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            HttpListenerResponse response = context.Response;
             response.ContentLength64 = buffer.Length;
             System.IO.Stream output = response.OutputStream;
             output.Write(buffer, 0, buffer.Length);
             output.Close();
+
+            List<string> ss = new List<string>();
+            ss.Add("Received HTTP request: ");
+            ss.Add("username: " + username);
+            ss.Add("remoteHost: " + remoteHost);
+            ss.Add("remotePort: " + remotePort);
+            ss.Add("\r\n");
+            ss.Add("Sent response: ");
+            ss.Add(responseString);
+            //Message.Inform(string.Join("\r\n", ss));
+
+            state.Text = string.Join("\r\n", ss);
+            start.Enabled = true;
         }
+        string remoteHost;
+        string remotePort;
     }
-
-    //public class ZeroConf: IZeroconfHost
-    //{
-
-    //}
 }
