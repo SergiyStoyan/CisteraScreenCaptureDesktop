5373eca29e0917620f0a820804b202661273ce68
cliversoft <sergey.stoyan@gmail.com>
2017-11-25 19:03:44 +0200

    .

--- CisteraScreenCaptureService/Program.cs
+++ CisteraScreenCaptureService/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
 
 namespace CisteraScreenCaptureService
 {
@@ -21,5 +22,27 @@ namespace CisteraScreenCaptureService
             };
             ServiceBase.Run(ServicesToRun);
         }
+
+        //[ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples", SessionMode = SessionMode.Required, CallbackContract = typeof(IClientApi))]
+        //public interface IServerApi
+        //{
+        //    [OperationContract(IsOneWay = true)]
+        //    void ReloadSetting();
+        //}
+        //public interface IClientApi
+        //{
+        //    [OperationContract(IsOneWay = true)]
+        //    void ServiceStarted();
+        //    [OperationContract(IsOneWay = true)]
+        //    void ServiceStopped();
+        //}
+
+        //[ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.PerCall)]
+        //public class CalculatorService : IServerApi
+        //{
+        //    public void ReloadSetting()
+        //    {
+        //    }
+        //}
     }
 }
